c00dabd47480aed3ed57a8b2f8614fb211e63c03
Yogesh Nikam <yogeshnikam671@gmail.com>
2022-09-30 12:10:42 +0530

    Yogesh | Change the way the response file is accessed in code

--- dummy-api/dummyApi.js
+++ dummy-api/dummyApi.js
@@ -1,5 +1,5 @@
 function inject(request, state, logger, callback) {
-    const response = require('/Users/yogesh/personal_projects/mountebank-first/dummy-api/response/success.json')
+    const response = require(process.cwd() + '/dummy-api/response/success.json')
     console.log('The response is --> ', response);
     callback(response);
 }
